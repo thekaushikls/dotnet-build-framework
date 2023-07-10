ac63c40e54825ad816d6a39b0ebe1023b6218f26
Kaushik <50703773+thekaushikls@users.noreply.github.com>
2023-07-10 09:07:51 +0200

    [added] sample vs project

---
+++ sample/HelloWorldSampleProject/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorldLibrary
+{
+    public static class HelloWorld
+    {
+        public static void Greet(string name)
+        {
+            if (name.ToLower() == "kaushik")
+            {
+                Console.WriteLine($"Welcome back {name}!\n");
+            }
+            else
+            {
+                Console.WriteLine($"Hello {name}\n");
+            }
+        }
+    }
+}
---
+++ sample/SampleConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SampleConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            HelloWorldLibrary.HelloWorld.Greet(name);
+
+            Console.ReadKey();
+        }
+    }
+}
